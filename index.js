39b8a27d653297c45c24a388e1dd37812d674c2a
fffaheem <mohdfaheemahmad5@gmail.com>
2026-06-17 20:16:19 +0530

    refactoring

--- index.js
+++ index.js
@@ -1,82 +1,54 @@
-// let footer = document.querySelectorAll(".footer");
-// let pg_count = 1;
-// footer.forEach(pg => {
-//     pg.innerText = pg_count;
-//     pg_count++;
-// });
+(() => {
+  let elements = {
+    body: document.querySelector("body"),
+    font: document.querySelectorAll("[class*='font-']"),
+    padding: document.querySelectorAll("[class*='p-']")
+  }
 
-let header = document.querySelector(".header");
-let footer = document.querySelector(".footer");
+  function set_padding() {
 
-let header_start = document.querySelectorAll(".header-start");
-let header_no_count = document.querySelectorAll(".header-no-count");
-let header_no = document.querySelectorAll(".header-no");
-let header_end = document.querySelectorAll(".header-end");
-
-let footer_start = document.querySelectorAll(".footer-start");
-let footer_no_count = document.querySelectorAll(".footer-no-count");
-let footer_no = document.querySelectorAll(".footer-no");
-let footer_end = document.querySelectorAll(".footer-end");
-
-if (header_start.length != header_end.length) {
-    alert("header start and end are not equal");
-}
-
-if (footer_start.length != footer_end.length) {
-    alert("footer start and end are not equal");
-}
-
-let listt = []
-for (let i = 0; i < header_start.length; i++) {
-    let start = header_start[i];
-    let end = header_end[i];
-    let inner_list = [start];
-    while (start != end) {
-        start = start.nextElementSibling;
-        inner_list.push(start);
+    for (const el of elements.padding) {
+      if (el === elements.body) {
+        continue;
+      }
+      
+      for(const cls of el.classList){
+        if (!cls.startsWith("p-")) {
+          continue
+        }
+        let paddings = cls.split("-").slice(1,-1);
+        if (paddings.length == 1) {
+          el.style.padding = `${paddings[0]}px`
+          // all left,right,up and down
+        } else if (paddings.length == 2) {
+          // left,right and up, down
+        } else if (paddings.length == 4) {
+          // left, right, up and down
+        } else {
+          prompt("abey")
+        }
+        console.log(paddings)
+      }
+      // process el
     }
-    listt.push(inner_list);
-    // console.log(inner_list);
-}
 
-console.log(listt);
+  }
 
-for (let i = 0; i < listt.length; i++) {
-    let inner_list = listt[i];
-    let counter = 0;
-    for (let j = 0; j < inner_list.length; j++) {
-        let el = inner_list[j];
-        let header_text = "";
-        if (el.className.includes("header-no-count")) {
-
-        } else if (el.className.includes("header-no")) {
-            counter++;
-        }
-        else {
-            counter++;
-            header_text = header.innerHTML + " " + counter;
-        }
-        el.innerHTML = header_text + "<br>" + el.innerHTML
-    }
-}
+  set_padding()
 
 
-
-
-
-// for font size
-let elements = document.querySelectorAll("[class*='font-']");
-
-elements.forEach(el => {
-    el.classList.forEach(cls => {
-        if (cls.startsWith("font-")) {
-            let size = cls.split("-")[1];
-            if (!isNaN(size)) {
-                el.style.fontSize = size + "px";
-            } else {
-                el.style.fontSize = "50px";
-                el.innerText = "Wrong font size class please fix it";
-            }
-        }
-    });
-});
+  elements.font.forEach(el => {
+      el.classList.forEach(cls => {
+          if (cls.startsWith("font-")) {
+              let size = cls.split("-")[1];
+              if (!isNaN(size)) {
+                  el.style.fontSize = size + "px";
+              } else {
+                  el.style.fontSize = "50px";
+                  el.innerText = "Wrong font size class please fix it";
+              }
+          }
+      });
+  });
+  
+})();
